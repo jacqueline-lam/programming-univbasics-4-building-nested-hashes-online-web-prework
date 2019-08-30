f1f038465fcbeeae63e0859bb4be1454dcf823ca
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-30 00:06:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
   {
 	  :railroads => {
 	    :pieces => 4,
-      :rent_in_dollar => {:one_piece_owned => 25, :one_piece_owned => 25, }
+      :rent_in_dollar => {:one_piece_owned => 25, :two_piece_owned => 50, :three_piece_owned => 100, :one_piece_owned => 200}
 	  }
 	}
 end
